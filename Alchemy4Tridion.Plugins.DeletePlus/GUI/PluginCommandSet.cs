612cfbfcb5c8d98378dca12e18390e2705cf461e
Mark Vlasenko <mvlasenko@sdl.com>
2016-02-29 16:42:14 +0200

    new project

---
+++ Alchemy4Tridion.Plugins.DeletePlus/GUI/PluginCommandSet.cs
@@ -0,0 +1,13 @@
+﻿using Alchemy4Tridion.Plugins.GUI.Configuration;
+
+namespace Alchemy4Tridion.Plugins.DeletePlus.GUI
+{
+    public class PluginCommandSet : Alchemy4Tridion.Plugins.GUI.Configuration.CommandSet
+    {
+        public PluginCommandSet()
+        {
+            // we only need to add the name of our command
+            AddCommand("SayHello");
+        }
+    }
+}
